d63444d227c048081448f1229b32caaf9b156414
Muhammad Saad Saif <gcsaadsaif123@gmail.com>
2026-02-25 00:09:47 +0500

    UI

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,47 +1,50 @@
-import React from "react";
+import {
+  FaInstagramSquare,
+  FaEnvelope,
+  FaLinkedin,
+  FaGithub,
+} from "react-icons/fa";
+
+const links = [
+  {
+    name: "Instagram",
+    path: "https://www.instagram.com/saadsaifsheikh/",
+    icon: FaInstagramSquare,
+  },
+  {
+    name: "Email",
+    path: "mailto:gcsaadsaif123@gmail.com",
+    icon: FaEnvelope,
+  },
+  {
+    name: "Linkedin",
+    path: "https://www.linkedin.com/in/muhammad-saad-saif-10b38a360/",
+    icon: FaLinkedin,
+  },
+  {
+    name: "Github",
+    path: "https://github.com/M-Saad-saif",
+    icon: FaGithub,},
+];
 
 export default function Footer() {
   return (
     <div>
       {/* Footer */}
       <footer className="home-footer">
-        <p className="copyright my-3 fs-6 text-white">Contect me if found something wrong</p>
+        <p className="copyright my-3 fs-6 text-white">
+          Contect me if found something wrong
+        </p>
         <div className="footer-links">
-          <a
-            href="https://www.instagram.com/saadsaifsheikh/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="footer-link"
-          >
-            <i className="ri-instagram-line"></i> Instagram
-          </a>
-          <a href="mailto:gcsaadsaif123@gmail.com" className="footer-link">
-            <i className="ri-mail-line"></i> Email
-          </a>
-          <a
-            href="https://www.linkedin.com/in/muhammad-saad-saif-10b38a360/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="footer-link"
-          >
-            <i className="fa-brands fa-linkedin-in"></i> Linkedin
-          </a>
-          <a
-            href="https://github.com/M-Saad-saif"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="footer-link"
-          >
-            <i className="fa-brands fa-github"></i>Github
-          </a>
-          <a
-            href="https://saadsaif.vercel.app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="footer-link"
-          >
-            <i className="ri-code-s-slash-line"></i>Portfolio
-          </a>
+          {links.map((link, index) => {
+            const Icon = link.icon;
+
+            return (
+              <a key={index} href={link.path} className="footer-link">
+                <Icon size={15} /> {link.name}
+              </a>
+            );
+          })}
         </div>
       </footer>
     </div>
